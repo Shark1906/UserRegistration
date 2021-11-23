e929ac55aeb9f927c18a666e3a1f8fed106cba96
Shark1906 <amanraj1906@gmail.com>
2021-11-24 01:44:04 +0530

    ADDED : UserRegistration_uc7

--- src/com/user/registration/ValidationOperation.java
+++ src/com/user/registration/ValidationOperation.java
@@ -7,7 +7,7 @@ public class ValidationOperation {
 	String patternName = "^([A-Z]{1}+[a-z]{2,})*$";
 	String patternEmail = "^[0-9a-zA-Z+-._]+@[-+_.0-9a-zA-Z]*.[a-zA-Z]{2,3}.([a-zA-z]{2,3})*$";
 	String patternPhone = "^([0-9]{1,2})\\s([0-9]{10})$";
-	String patternPassword = "^(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+	String patternPassword = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 	
 	User user = new User();
 	Scanner sc = new Scanner(System.in);
